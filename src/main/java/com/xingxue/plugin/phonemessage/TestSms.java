d53af6dc9d492407aee9d6204fe802ed898edd26
FZONEPIECE <1277417298@qq.com>
2017-09-20 14:29:36 +0800

    javaMail优化

--- src/main/java/com/xingxue/plugin/phonemessage/TestSms.java
+++ src/main/java/com/xingxue/plugin/phonemessage/TestSms.java
@@ -6,6 +6,19 @@ package com.xingxue.plugin.phonemessage;
 * 4.设置短信签名，短信模板（2小时的审核）
 * 5.充值
 * 6.获取accesskeys和密码
+* 7.加入maven依赖
+mvn install:-file -Dfile=C:\aliyun-java-sdk-core-3.2.3.jar -DgroupId=com.aliyun -DartifactId=aliyun-java-sdk-core -Dversion=3.2.3 -Dpackaging=jar
+mvn install:install-file -Dfile=C:\aliyun-java-sdk-dysmsapi-1.0.0-SANPSHOT.jar -DgroupId=com.aliyun -DartifactId=aliyun-java-sdk-dysmsapi -Dversion=1.0.0 -Dpackaging=jar
+    <dependency>
+      <groupId>com.aliyun</groupId>
+      <artifactId>aliyun-java-sdk-core</artifactId>
+      <version>3.2.3</version>
+    </dependency>
+    <dependency>
+      <groupId>com.aliyun</groupId>
+      <artifactId>aliyun-java-sdk-dysmsapi</artifactId>
+      <version>1.0.0</version>
+    </dependency>
 * */
 public class TestSms {
 
