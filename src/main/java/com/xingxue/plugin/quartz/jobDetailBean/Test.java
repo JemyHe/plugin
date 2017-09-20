69b5be20bf6eb046ef47acdc8f7a260ecb9f7a24
JeremyHe <546016981@qq.com>
2017-09-20 14:40:50 +0800

    Merge branch 'master' of https://github.com/JemyHe/plugin
    # Conflicts:
    #	.idea/vcs.xml
    #	.idea/workspace.xml
    #	src/main/java/com/xingxue/plugin/javamail/service/SendMailService.java
    #	src/main/java/com/xingxue/plugin/javamail/service/TestHtmlMail.java

---
+++ src/main/java/com/xingxue/plugin/quartz/jobDetailBean/Test.java
@@ -0,0 +1,11 @@
+package com.xingxue.plugin.quartz.jobDetailBean;
+
+/**
+ * Created by Administrator on 2017/9/20.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
